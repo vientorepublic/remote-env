9346a45d9d87708abb46ef18f062dff268b0cc00
vientorepublic <markgod0419@gmail.com>
2024-05-25 18:01:15 +0900

    ✅ update: add jsdoc

--- src/client.ts
+++ src/client.ts
@@ -1,22 +1,45 @@
 import { connect, Socket } from 'net';
 import consola from 'consola';
 
+/**
+ * Remote-env client instance. To connect to a server, call `connect()` and provide server information in the parameter.
+ * @author Doyeon Kim - https://github.com/vientorepublic
+ */
 export class remoteEnvClient {
   public client: Socket;
 
+  /**
+   * Connect to remote-env server.
+   * @example const { remoteEnvClient } = require('@vientorepublic/remote-env');
+   * const client = new remoteEnvClient();
+   * client.connect('127.0.0.1', 8080);
+   * @param { string } address
+   * @param { port } port
+   * @author Doyeon Kim - https://github.com/vientorepublic
+   */
   public connect(address: string, port: number): void {
     this.client = connect({ host: address, port: port }, () => {
       consola.success('New remote-env client created!');
     });
   }
 
+  /**
+   * Close the connection with server.
+   * @author Doyeon Kim - https://github.com/vientorepublic
+   */
   public close(): void {
     this.client.end(() => {
       consola.info('remote-env client connection closed.');
     });
   }
 
-  public getEnv(key: string): Promise<string | undefined> {
+  /**
+   * Requests the environment variable value corresponding to the provided key.
+   * @param { string } key
+   * @returns { Promise<string> }
+   * @author Doyeon Kim - https://github.com/vientorepublic
+   */
+  public getEnv(key: string): Promise<string> {
     return new Promise((resolve, reject) => {
       this.client.write(key);
       this.client.on('error', (err) => reject(err));
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,27 @@
 import { config } from 'dotenv';
-import { createServer } from 'net';
+import { createServer, Server } from 'net';
 import consola from 'consola';
 
+/**
+ * Remote-env server instance. To open a server, call `createServer()`
+ * @author Doyeon Kim - https://github.com/vientorepublic
+ */
 export class remoteEnvProvider {
   public path?: string;
   public port: number;
   public addr: string;
+  public server: Server;
+  /**
+   * @param { string } addr
+   * @param { number } port
+   * @param { string } path
+   */
   constructor(addr: string, port: number, path?: string) {
     this.path = path;
     this.port = port;
     this.addr = addr;
     config({ path: this.path ?? null });
-  }
-
-  public getEnv(key: string): string | undefined {
-    return process.env[key];
-  }
-
-  public async createServer() {
-    createServer((socket) => {
+    this.server = createServer((socket) => {
       const address = socket.remoteAddress;
       const port = socket.remotePort;
       consola.success(`New remote-env client connected!`);
@@ -29,15 +32,31 @@ export class remoteEnvProvider {
         const value = this.getEnv(key);
         if (value) {
           socket.write(value);
-        } else {
-          socket.write('Failed to get env value');
         }
       });
 
       socket.on('close', () => {
         consola.info('remote-env client disconnected.');
       });
-    }).listen(this.port, this.addr, () => {
+    });
+  }
+
+  /**
+   * Get env variable from [Dotenv](https://www.npmjs.com/package/dotenv).
+   * @param { string } key
+   * @returns { string | undefined }
+   * @author Doyeon Kim - https://github.com/vientorepublic
+   */
+  private getEnv(key: string): string | undefined {
+    return process.env[key];
+  }
+
+  /**
+   * Create a server from the declared instance.
+   * @author Doyeon Kim - https://github.com/vientorepublic
+   */
+  public async createServer() {
+    this.server.listen(this.port, this.addr, () => {
       consola.ready({
         message: `remote-env server listening on ${this.addr}:${this.port}`,
         badge: true,
